92199b32dfff172b273cdd6b4a16cfd9c33caf84
hamzathabet <benthabethamza7@gmail.com>
2022-03-12 11:17:29 +0100

    jsx checkpoint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "./style.css";
+import imageInSrc from "./assets/imageInSrc.jpg";
+import videoInsrc from "./assets/myVideo.mp4";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div style={{background:'#e5e5e5'}}>
+      <div className="container">
+        <div style={{ border: "solid 1px white", maxWidth: "100vw" }} className={'profile'}>
+        <div className="container">
+          <h1 className={"title red"}>Hamza Ben Thabet</h1>
+          <br />
+          <img src={imageInSrc} alt="image src" />
+          <br />
+          <img src="./imageInPublic.png" alt="image public" />
+          </div>
+        </div>
+
+        <video width={320} height={240} controls>
+          <source src={videoInsrc} type="video/mp4" />
+        </video>
+      </div>
+      </div>
+    </>
   );
 }
 
